afe379f270a643e11a16fddf78791f5f679fff84
ikram9820 <ikram9820khan@gmail.com>
2022-03-01 10:35:59 -0800

    ch7

--- config/settings.py
+++ config/settings.py
@@ -15,8 +15,6 @@ AUTH_USER_MODEL = 'accounts.CustomUser'
 LOGIN_REDIRECT_URL= 'home'
 LOGOUT_REDIRECT_URL='home'
 
-CRISPY_TEMPLATE_PACK= 'bootstrap4'
-
 STATIC_URL = '/static/'
 STATICFILES_DIRS= (str(BASE_DIR.joinpath('static')),)
 STATIC_ROOT= str(BASE_DIR.joinpath('staticfiles'))
@@ -24,6 +22,18 @@ STATICFILES_FINDERS= [
     "django.contrib.staticfiles.finders.FileSystemFinder",
     "django.contrib.staticfiles.finders.AppDirectoriesFinder",
 ]
+
+CRISPY_TEMPLATE_PACK= 'bootstrap4'
+
+SITE_ID= 1
+AUTHENTICATION_BACKENDS = (
+'django.contrib.auth.backends.ModelBackend',
+'allauth.account.auth_backends.AuthenticationBackend',
+)
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+ACCOUNT_LOGOUT_REDIRECT= 'home'
+ACCOUNT_SESSION_REMEMBER = True 
+ACCOUNT_SIGNUP_PASSWORD_ENTER_TWICE = False
 
 
 INSTALLED_APPS = [
@@ -33,8 +43,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
 
     'crispy_forms',
+    'allauth',
+    'allauth.account',
 
     'accounts',
     'pages',
@@ -74,7 +87,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
-
 # Database
 DATABASES = {
     'default': {
@@ -88,10 +100,7 @@ DATABASES = {
         }
 }
 
-
 # Password validation
-# https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
