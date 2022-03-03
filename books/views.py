cba16e2e0b2351eface50af0d91b72a2258599ad
ikram9820 <ikram9820khan@gmail.com>
2022-03-03 09:48:30 -0800

    ch14 without test

--- books/views.py
+++ books/views.py
@@ -1,4 +1,5 @@
 from django.views.generic import ListView,DetailView
+from django.db.models import Q
 from django.contrib.auth.mixins import LoginRequiredMixin,PermissionRequiredMixin
 from .models import Book
 
@@ -15,3 +16,21 @@ class BookDetailView(LoginRequiredMixin,PermissionRequiredMixin,DetailView):
     login_url= 'accoutn_login'
     permission_required= 'books.special_status'
     
+
+class SearchResultsListView(ListView):
+    model= Book
+    context_object_name='books'
+    tempalte_name='books/search_results.html'
+    def get_queryset(self):
+        query= self.request.GET.get('q')
+        return Book.objects.filter(
+            Q(title__icontains=query)| Q(title__icontains=query))
+
+
+
+
+
+
+
+
+
