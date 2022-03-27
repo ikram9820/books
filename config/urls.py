de9c3836adf351c9f1da50329cc26c82d981aa02
ikram9820 <ikram9820khan@gmail.com>
2022-03-27 07:15:57 -0700

    setting.MEDIA_URL updated to settings.common.MEDIA_URL in config.urls

--- config/urls.py
+++ config/urls.py
@@ -9,4 +9,4 @@ urlpatterns = [
     path('accounts/',include('accounts.urls')),
     path('__debug__/', include('debug_toolbar.urls')),
     path('',include('books.urls')),
-] + static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
+] + static(settings.common.MEDIA_URL, document_root= settings.common.MEDIA_ROOT)
