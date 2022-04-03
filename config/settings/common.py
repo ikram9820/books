75768fd62c88c3288a13c286b8c4d227e35f54aa
ikram9820 <ikram9820khan@gmail.com>
2022-04-03 04:55:57 -0700

    config is updated

--- config/settings/common.py
+++ config/settings/common.py
@@ -54,6 +54,7 @@ MIDDLEWARE = [
 
     "debug_toolbar.middleware.DebugToolbarMiddleware",
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
