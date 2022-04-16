e20f09786e82fc70b54452afe986e95407f7d1bf
ikram9820 <ikram9820khan@gmail.com>
2022-04-16 02:43:05 -0700

    STATIC  update

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -13,7 +13,7 @@ ALLOWED_HOSTS = [os.environ['DJANGO_ALLOWED_HOSTS']]
 AWS_ACCESS_KEY_ID= os.environ['AWS_ACCESS_KEY_ID']
 AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
 AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
-AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 AWS_DEFAULT_ACL = None
 AWS_S3_OBJECT_PARAMETERS = {'CacheControl':'max-age-86400'}
 AWS_S3_FILE_OVERWRITE = False
@@ -21,7 +21,10 @@ AWS_LOCATION = 'static'
 AWS_HEADER = {'Access-Control-Allow-Origin':'*'}
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
-STATIC_URL= 'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 MEDIA_URL= 'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
 
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage' 
