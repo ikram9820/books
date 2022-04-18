bece4e47c6708de10026bf48ea62de2d58e9b39a
ikram9820 <ikram9820khan@gmail.com>
2022-04-18 02:28:48 -0700

    v1.0.1

--- config/urls.py
+++ config/urls.py
@@ -3,6 +3,10 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+admin.site.site_header = 'Books Admin'
+
+admin.site.index_title = 'Admin'
+
 urlpatterns = [
     path('anything-but-admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
