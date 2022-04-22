60469a020e2443b5ed56038a2557f8408ccb9270
Ikram khan <94529450+ikram9820@users.noreply.github.com>
2022-04-22 20:31:51 +0500

    Update models.py

--- books/models.py
+++ books/models.py
@@ -8,6 +8,8 @@ import uuid
     
 class Category(models.Model):
     name = models.CharField(max_length=150)
+    def __str__(self):
+        return self.name
 
 class Book(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
