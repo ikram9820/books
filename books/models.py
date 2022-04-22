bfcf56e884714fc1696622acc928324b86dde4d5
Ikram khan <94529450+ikram9820@users.noreply.github.com>
2022-04-22 22:31:54 +0500

    Update models.py

--- books/models.py
+++ books/models.py
@@ -10,6 +10,8 @@ class Category(models.Model):
     name = models.CharField(max_length=150,unique=True)
     def __str__(self):
         return self.name
+    class Meta:
+        ordering = ['name']
 
 class Book(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
