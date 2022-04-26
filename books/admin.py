7958640ce293ce5f3f4d5bdd9b8dd956094d5a4c
Ikram khan <94529450+ikram9820@users.noreply.github.com>
2022-04-27 04:08:05 +0500

    Update admin.py

--- books/admin.py
+++ books/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import Book,Favorite,FavoriteBook , Category
+from .models import Book,Favorite,FavoriteBook , Genre
 
-admin.site.register(Category)
+admin.site.register(Genre)
 class BookAdmin(admin.ModelAdmin):
     list_display= ('title','user','author','is_visible','size')
     
